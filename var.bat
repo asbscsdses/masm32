2de39adaff14a743aaecb708cf8372bc133f0f3b
asbscsdses <779954817@qq.com>
2016-10-20 14:03:21 +0800

    make
    add makefile

--- var.bat
+++ var.bat
@@ -1,5 +1,5 @@
 @echo off
 set include=d:\masm32\include
 set lib=d:\masm32\lib
-set path=d:\masm32\bin;%path%
+set path=d:\masm32\bin;D:\Program Files\Microsoft Visual Studio 14.0\VC\bin;%path%
 echo on
