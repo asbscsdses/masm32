f1046f083e693c83f6c245966767f702d4bbc8fe
asbscsdses <779954817@qq.com>
2016-10-22 10:26:55 +0800

    acer bat
    acer

---
+++ var__acer.bat
@@ -0,0 +1,5 @@
+@echo off
+set include=d:\masm32\include;C:\Program Files (x86)\Windows Kits\8.1\Include\um;C:\Program Files (x86)\Windows Kits\8.1\Include\shared
+set lib=d:\masm32\lib
+set path=d:\masm32\bin;D:\Pm\vs2015\VC\bin;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin;%path%
+echo on
